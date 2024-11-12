bb4d272f1015e88adbbd54e5eae4d7a4ab499251
Aboubacar <58235631+aboubacar3012@users.noreply.github.com>
2024-11-12 10:46:17 +0000

    Ajout d'une fonction de filtrage et d'un exemple d'utilisation

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 console.log("Hello, World!");
+// test function 
+const someFunction = () => {
+  console.log("Hello, World!");
+}
+
+const filter = (arr, callback) => {
+  const result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (callback(arr[i])) {
+      result.push(arr[i]);
+    }
+  }
+  return result;
+}
+
+const arr = [1, 2, 3, 4, 5];
+const filteredArr = filter(arr, (num) => num % 2 === 0);
+console.log(filteredArr); // [2, 4]
